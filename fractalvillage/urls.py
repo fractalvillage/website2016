a6d6c773ecadcbc755e2929a3519968a2825fc05
Omer Zak <w1@zak.co.il>
2016-02-21 13:07:50 +0200

    Now properly serving also user-uploaded files.

--- fractalvillage/urls.py
+++ fractalvillage/urls.py
@@ -15,6 +15,9 @@ Including another URLconf
 """
 from django.conf.urls import url, include
 from django.contrib import admin
+from django.conf import settings
+from django.conf.urls.static import static
+
 from rest_framework import routers
 from fractalvillage.crud import views
 
@@ -26,4 +29,4 @@ router.register(r'camps', views.CampViewSet)
 urlpatterns = [
     url(r'^', include(router.urls)),
     url(r'^admin/', admin.site.urls),
-]
+] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
