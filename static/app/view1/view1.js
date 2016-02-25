f9a8b9736ed41f17abe0c6cdd80788a596c8d759
Omer Zak <w1@zak.co.il>
2016-02-26 01:44:43 +0200

    Tweaked the AngularJS code until it actually does something i.e. display the basic Fractal Village information.

--- static/app/view1/view1.js
+++ static/app/view1/view1.js
@@ -9,6 +9,16 @@ angular.module('myApp.view1', ['ngRoute'])
   });
 }])
 
-.controller('View1Ctrl', [function() {
+.controller('View1Ctrl', ['$scope', 'getVillages', function($scope,getVillages) {
+  getVillages(function(villages) {
+    $scope.villages = villages;
+  });
+}])
 
+.service('getVillages', ['$http', function($http) {
+  return function(callback) {
+    return $http.get('v1/villages').then(function(response) {
+      callback(response.data.results);
+    });
+  };
 }]);
